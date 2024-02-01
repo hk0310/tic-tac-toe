893cf0b598df9f14ac6fe700675998f23e5fbbfa
HKhai <hoangkhai.nt@outlook.com>
2024-02-01 14:03:25 -0600

    Dehardcoded the playboard

--- src/App.js
+++ src/App.js
@@ -27,26 +27,22 @@ function Board({ board, xIsNext, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  let boardJsx = [];
+  for(let i = 0; i < 3; i++) {
+    let squares = []
+    for(let j = 0; j < 3; j++) {
+      squares = [...squares, <Square value={board[3 * i + j]} onSquareClick={() => handleClick(3 * i + j)} />]
+    }
+    let row = <div className="board-row">{squares}</div>;
+    boardJsx = [...boardJsx, row];
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={board[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={board[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={board[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={board[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={board[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={board[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={board[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={board[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={board[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardJsx}
     </>
-  );
+  )
 }
 
 export default function Game() {
